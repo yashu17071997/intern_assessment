2ade2ecefc121a23b90f427d95fd764c3bb1e861
yashu17071997 <68971799+yashu17071997@users.noreply.github.com>
2020-12-18 00:33:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-from flask_restful import Api, Resource, reqparse, request
-from flask import Flask
+"""
+to create simple api in flask framework
+"""
 import re
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, request
 
-app = Flask(__name__)
-api = Api(app)
-
+APP = Flask(__name__)
+API = Api(APP)
 
 class Sanitize(Resource):
-
-
+    """  to create rest api for http post """
     request_parser = reqparse.RequestParser()
 
-    def checkForSqlInjectChar(inputString):
-        return re.search(r'^[a-z0-9A-Z]*$', inputString)
+    def check_for_sql_inject_char(input_string):
+        """To check for sql inject char for the given input """
+        return re.search(r'^[a-z0-9A-Z]*$', input_string)
 
     def post(self):
         data = request.get_json()
-        inputString = data['payload']
-        if inputString == "":
+        input_string = data['payload']
+        if input_string == "":
+            return 'bad request!', 400
 
-          return 400
-        isSanitized = Sanitize.checkForSqlInjectChar(inputString)
-        if isSanitized:
+        is_sanitized = Sanitize.check_for_sql_inject_char(input_string)
+        if is_sanitized:
             return {'result': "sanitized"}
         return {'result': "unsanitized"}
 
-api.add_resource(Sanitize, '/v1/sanitized/input')
+
+API.add_resource(Sanitize, '/v1/sanitized/input')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    APP.run(port=5000, debug=True)
